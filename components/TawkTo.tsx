8bbe8beaae71139e632ccd644867cd6dea8aae98
Afrizal Aminulloh <afrizal.aminuloh@gmail.com>
2025-07-16 13:30:16 +0700

    Create TawkTo.tsx

---
+++ components/TawkTo.tsx
@@ -0,0 +1,18 @@
+import { useEffect } from 'react';
+
+const TawkTo = () => {
+  useEffect(() => {
+    if (typeof window === 'undefined') return;
+
+    const script = document.createElement("script");
+    script.src = "https://embed.tawk.to/6877300a7f202b19181ec1b3/1j08p7b5r";
+    script.async = true;
+    script.charset = "UTF-8";
+    script.setAttribute("crossorigin", "*");
+    document.body.appendChild(script);
+  }, []);
+
+  return null;
+};
+
+export default TawkTo;
